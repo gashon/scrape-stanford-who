0665d6d0340e5ead8390c615b24297cacf4151ad
GashonHussein <gashon96@gmail.com>
2023-03-02 01:00:09 -0800

    fix: repo org

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo $(ls | grep csv | xargs -I{} cat {} | uniq) > $(date +%Y-%m-%d).csv
-
 old_csv=$(ls | grep csv | tr '\n' ' ')
 dir=$(date +%Y-%m-%d)
 
+mkdir $dir
 mv $old_csv $dir/
 mv $dir archives/
 
-cd spider && go run main.go
+# cd spider && go run main.go
+echo $(ls | grep csv | xargs -I{} cat {} | uniq) > $(date +%Y-%m-%d).csv
 
 cd ../
 git pull
