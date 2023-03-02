d472f867823f34dda9202b5d66a2a76f3b0e670f
GashonHussein <gashon96@gmail.com>
2023-03-02 00:57:36 -0800

    refactor: archives repo organization

--- scripts/scrape.sh
+++ scripts/scrape.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo $(ls | grep csv | xargs -I{} cat {} | uniq) > $(date +%Y-%m-%d).csv
+
 old_csv=$(ls | grep csv | tr '\n' ' ')
-mv $old_csv archives/
+dir=$(date +%Y-%m-%d)
+
+mv $old_csv $dir/
+mv $dir archives/
 
 cd spider && go run main.go
 
